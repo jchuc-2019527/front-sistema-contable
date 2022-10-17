c43ec6247c76ac8e0ab240874762c280c9195845
jchuc-2019527 <jchuc-2019527@kinal.edu.gt>
2022-10-17 17:01:00 -0600

    Perfil

--- src/Components/Perfil/Perfil.jsx
+++ src/Components/Perfil/Perfil.jsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import React, {useState,useEffect} from 'react'
 import './Perfil.css'
 import logoP from '../../Shared/img/profile-user.svg';
+import { Link } from 'react-router-dom';
+import Axios from 'axios';
+import Swal from 'sweetalert2';
+import { useNavigate } from 'react-router-dom';
+import {url} from '../axiosConnect';
+
 
 const Perfil = () => {
+
+    const navigate = useNavigate();
+    const [perfil, setPerfil] = useState([]);
+    const [user, setUser] = useState([]);
+    const headers = {
+        headers: {
+            Authorization: `${JSON.parse(localStorage.getItem("token"))}`,
+        }
+    }
+
+    useEffect(() => {
+        Axios.get(url +'user/profile', headers)
+        .then( (res) => {
+            console.log(res.data)
+        })
+        .catch( (err) =>{
+            console.log(err, 'Viene el error')
+        })
+    }, []);
+
   return (
     <div>
    <body class="profile">
@@ -23,7 +49,7 @@ const Perfil = () => {
                     <p>Nombre Empresa</p>
                 </div>
             </div>
-            <a  class="btn" >Regresar</a>
+           <Link to={'/tareas'}> <a class="btn" >Regresar</a> </Link>
         </div>
     </div>
 </body> 
