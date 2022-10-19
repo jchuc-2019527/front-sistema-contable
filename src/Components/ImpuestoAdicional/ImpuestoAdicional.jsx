16eb90dce703b76df1d8f2a0d6cede4e87f819c0
jchuc-2019527 <jchuc-2019527@kinal.edu.gt>
2022-10-19 16:50:57 -0600

    Register empresa

--- src/Components/ImpuestoAdicional/ImpuestoAdicional.jsx
+++ src/Components/ImpuestoAdicional/ImpuestoAdicional.jsx
@@ -1,9 +1,181 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import "./ImpuestoAdicional.css";
+import { url } from "../axiosConnect";
+import Axios from "axios";
+import Swal from "sweetalert2";
 
 const ImpuestoAdicional = () => {
-  return (
-    <div>ImpuestoAdicional</div>
-  )
-}
+  const [impuestos, setImpuestos] = useState([]);
+  const [data, setData] = useState({
+    nombreImpuesto: " ",
+    descripcionImpuesto: " ",
+    valorImpuesto: " ",
+  });
 
-export default ImpuestoAdicional
+  const headers = {
+    headers: {
+      Authorization: `${JSON.parse(localStorage.getItem("token"))}`,
+    },
+  };
+
+  useEffect(() => {
+    Axios.get(url + "impuesto/taxes", headers)
+      .then((res) => {
+        console.log("Vienen los impuestos", res.data.result);
+        setImpuestos(res.data.result);
+      })
+      .catch((err) => {
+        console.log("Viene el error", err);
+      });
+  }, []);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setData({
+      ...data,
+      [name]: value,
+    });
+  };
+
+  const agregar = (e) => {
+    e.preventDefault();
+    Axios.post(url + "impuesto/newImpuesto", data, headers)
+      .then((res) => {
+        Swal.fire({
+          position: "center",
+          icon:'success',
+          color: "white",
+          background: "rgba(0,0,0,0.9)",
+          title: "Impuesto agregado satisfactoriamente",
+          timer: 5000,
+          backdrop: true,
+          timerProgressBar: true,
+          toast: true
+        });
+        e.target.reset();
+      })
+      .catch((err) => {
+        Swal.fire({
+          position: "center",
+          icon:'warning',
+          color: "white",
+          background: "rgba(0,0,0,0.9)",
+          title: err.response.data.message,
+          timer: 4000,
+          backdrop: true,
+          timerProgressBar: true,
+          toast: true
+        });
+      });
+  };
+
+  return (
+    <div>
+      <nav className="navbar bg-dark">
+        <div className="container-fluid">
+          <a className="btn">Menu Principal</a>
+          <form className="d-flex" role="search">
+            <input
+              className="form-control me-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+            <button className="btn btn-outline-success" type="submit">
+              Search
+            </button>
+          </form>
+        </div>
+      </nav>
+      <body>
+        <div className="container my-3">
+          <div className="row">
+            <div className="col-sm-12 col-md-4 col-lg-4 col-xl-4 py-4">
+              <h2>Nuevo Impuesto</h2>
+              <form onSubmit={agregar}>
+                <div className="mb-3">
+                  <label className="form-label">Nombre Impuesto</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="nombreImpuesto"
+                    name="nombreImpuesto"
+                    onChange={handleChange}
+                    required
+                  />
+                </div>
+                <div className="mb-3">
+                  <label className="form-label">Descripcion impuesto</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="descripcionImpuesto"
+                    name="descripcionImpuesto"
+                    onChange={handleChange}
+                    required
+                  />
+                </div>
+                <div className="mb-3">
+                  <label className="form-label">Valor Impuesto</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="valorImpuesto"
+                    name="valorImpuesto"
+                    onChange={handleChange}
+                    required
+                  />
+                </div>
+                <div className="d-grid gap-2">
+                  <button className="btn btn-success">Guardar</button>
+                </div>
+                <div className="btn-home"></div>
+              </form>
+            </div>
+            <div className="col-sm-12 col-md-8 col-lg-8 col-xl-8 py-4 ">
+              <div className="tab-content" id="myTabContent">
+                <div
+                  className="tab-pane fade show active"
+                  id="list"
+                  role="tabpanel"
+                  aria-labelledby="list-tab"
+                >
+                  <div className="card">
+                    <div className="card-header">
+                      <h4>Impuesto Adicional</h4>
+                    </div>
+                    <div className="card-body">
+                      <div className="table-responsive">
+                        <table
+                          id="userList"
+                          className="table table-bordered table-hover table-striped"
+                        >
+                          <thead className="thead-light">
+                            <tr>
+                              <th className="centrado">Nombre Impuesto</th>
+                              <th className="centrado">Descripcion Impuesto</th>
+                              <th className="centrado">Valor Impuesto</th>
+                            </tr>
+                          </thead>
+                          <tbody>
+                            <tr>
+                              <td className="centrado">IVA</td>
+                              <td className="centrado">IVA de un producto</td>
+                              <td className="centrado">4856</td>
+                            </tr>
+                          </tbody>
+                        </table>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </body>
+    </div>
+  );
+};
+
+export default ImpuestoAdicional;
--- src/Components/RegisterEmpresa/RegisterEmpresa.jsx
+++ src/Components/RegisterEmpresa/RegisterEmpresa.jsx
@@ -1,9 +1,147 @@
-import React from 'react'
+import React, { useState } from "react";
+import "./RegisterEmpresa.css";
+import Axios from "axios";
+import Swal from "sweetalert2";
+import { url } from "../axiosConnect";
+import { useNavigate } from "react-router-dom";
 
 const RegisterEmpresa = () => {
-  return (
-    <div>RegisterEmpresa</div>
-  )
-}
+  const navigate = useNavigate();
+  const [company, setCompany] = useState({
+    nombreEmpresa: "",
+    nombreComercial: "",
+    direccionEmpresa: "",
+    NITEmpresa: "",
+    porcentajeIva: "",
+  });
 
-export default RegisterEmpresa
+  const headers = {
+    headers: {
+      Authorization: `${JSON.parse(localStorage.getItem("token"))}`,
+    },
+  };
+
+  const handleEmpresa = (e) => {
+    const { name, value } = e.target;
+    setCompany({
+      ...company,
+      [name]: value,
+    });
+  };
+
+  const agregarEmpresa = (e) => {
+    e.preventDefault();
+    Axios.post(url + "empresaMaestro/newEmpresaMaestro", company, headers)
+      .then((res) => {
+        Swal.fire({
+          position: "center",
+          icon:'success',
+          color: "white",
+          background: "rgba(0,0,0,0.9)",
+          title: "Impuesto agregado satisfactoriamente",
+          timer: 5000,
+          backdrop: true,
+          timerProgressBar: true,
+          toast: true
+        });
+        e.target.reset();
+      })
+      .catch((err) => {
+        Swal.fire({
+          position: "center",
+          icon:'warning',
+          color: "white",
+          background: "rgba(0,0,0,0.9)",
+          title: err.response.data.message,
+          timer: 4000,
+          backdrop: true,
+          timerProgressBar: true,
+          toast: true
+        });
+      });
+  };
+
+  return (
+    <body className="register">
+      <div className="container">
+        <form onSubmit={agregarEmpresa}>
+          <div className="form first">
+            <div className="details personal">
+              <span className="title">Detalles</span>
+
+              <div className="fields">
+                <div className="input-field">
+                  <label>Nombre Empresa</label>
+                  <input
+                    type="text"
+                    placeholder="Ingrese el nombre"
+                    id="nombreEmpresa"
+                    name="nombreEmpresa"
+                    onChange={handleEmpresa}
+                    required
+                  />
+                </div>
+
+                <div className="input-field">
+                  <label>Nombre Comercial</label>
+                  <input
+                    type="text"
+                    placeholder="Ingrese el nombre"
+                    id="nombreComercial"
+                    name="nombreComercial"
+                    onChange={handleEmpresa}
+                    required
+                  />
+                </div>
+
+                <div className="input-field">
+                  <label>Direcciòn Empresa</label>
+                  <input
+                    type="text"
+                    placeholder="Ingrese la Direcciòn"
+                    id="direccionEmpresa"
+                    name="direccionEmpresa"
+                    onChange={handleEmpresa}
+                    required
+                  />
+                </div>
+
+                <div className="input-field">
+                  <label>NIT Empresa</label>
+                  <input
+                    type="text"
+                    placeholder="Ingrese el NIT"
+                    id="NITEmpresa"
+                    name="NITEmpresa"
+                    onChange={handleEmpresa}
+                    required
+                  />
+                </div>
+                <div className="input-field">
+                  <label>Porcentaje IVA</label>
+                  <input
+                    type="number"
+                    step="any"
+                    placeholder="Ingrese el Porcentaje"
+                    id="porcentajeIVA"
+                    name="porcentajeIVA"
+                    onChange={handleEmpresa}
+                    required
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="buttons">
+              <button className="sumbit">
+                <span className="btnText">Agregar</span>
+                <i className="uil uil-navigator"></i>
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </body>
+  );
+};
+
+export default RegisterEmpresa;
